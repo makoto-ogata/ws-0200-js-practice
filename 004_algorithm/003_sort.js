735c4b497b0fea23464dffb091ad1d503435bbbf
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-02-18 22:30:12 +0900

    Answered bubble sort question

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -9,6 +9,15 @@
  */
 
 function bubbleSort (array) {
+  for(let i = 0; i < array.length; i++){
+    for(let j = array.length - 1; i < j ; j--){
+      if(array[j] < array[j - 1]){
+        let tmp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = tmp;
+      }
+    }
+  }
   return array
 }
 
