8234bc8bd13981684efe67adbd3ff70f320ab80a
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-02-27 00:19:25 +0900

    Answered merge sort question

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -56,10 +56,30 @@ function insertSort (array) {
  */
 
 function mergeSort (arr) {
-  return arr
+  if(arr.length <= 1){
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  let array = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if(left[leftIndex] < right[rightIndex]){
+      array.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 /**
